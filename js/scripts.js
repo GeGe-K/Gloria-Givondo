725b73b54005d13a7469d08b9ec81154fd0b19c6
GeGe-K <gloriagivondo@gmail.com>
2019-02-19 16:56:59 +0300

    added captions to the projects

--- js/scripts.js
+++ js/scripts.js
@@ -70,4 +70,25 @@
         }
     });
 
-})(jQuery); 
+})(jQuery);
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('ip');
+var modalImg = document.getElementById("ip1");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
